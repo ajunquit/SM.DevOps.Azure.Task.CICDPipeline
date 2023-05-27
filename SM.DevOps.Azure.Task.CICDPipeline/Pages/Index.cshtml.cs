99194303e4e5f67d37d3cb1268eabdc57fe94d74
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2023-05-27 10:58:34 -0500

    Add project files.

---
+++ SM.DevOps.Azure.Task.CICDPipeline/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SM.DevOps.Azure.Task.CICDPipeline.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
